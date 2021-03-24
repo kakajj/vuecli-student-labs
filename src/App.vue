8e1a2bf38d7a9f661584438dc1566183c391e1d8
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-03-24 20:51:43 +0700

    success  l2-basic-form-start

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,38 @@
 
       <div class="container">
         <div class="survey-container">
-          <form action="">
+
+          <form @submit.prevent="submit">
             <div>
-              <h2 class="heading">
-                How was your Vue.js learning experience?
-              </h2>
+              <h2 class="heading">How was your Vue.js learning experience?</h2>
               <label class="label" for="name">Your Name</label>
-              <input class="input" id="name" type="text" />
+              <input v-model="myName" class="input" :class="{ errorinput: isErr}" id="name" type="text"  />
+              <p class="error" v-if="nameError">{{nameError}}</p>
             </div>
 
             <div>
               <h2 class="heading">My learning experience was ...</h2>
 
               <div>
-                <input type="radio" name="rating" id="rating-poor" />
+                <input type="radio" name="rating" id="rating-poor"  value="Poor" v-model="exp" />
                 <label class="label" for="rating-poor">Poor</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-avg" />
+                <input type="radio" name="rating" id="rating-avg" value="Average" v-model="exp" />
                 <label class="label" for="rating-avg">Average</label>
               </div>
 
               <div>
-                <input type="radio" name="rating" id="rating-great" />
+                <input type="radio" name="rating" id="rating-great" value="Great" v-model="exp" />
                 <label class="label" for="rating-great">Great</label>
               </div>
             </div>
-            <button class="btn">
-              Submit
-            </button>
+            <p class="error" v-if="inputError">{{inputError}}</p>
+            <p v-if="sucCessMessage">{{sucCessMessage}}</p>
+            <button class="btn">Submit</button>
           </form>
+
         </div>
       </div>
     </div>
@@ -46,12 +47,49 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      myName: "",
+      exp: "",
+      sucCess: null,
+      sucCessMessage: "",
+      nameError: "",
+      inputError: "",
+      isErr: false,
+    };
+  },
+  methods: {
+    submit() {
+      this.error = [];
+      this.sucCessMessag = "";
+      this.nameError = "";
+      this.inputError = "";
+      this.isErr = false;
+      if (this.myName && this.exp) {
+        this.sucCess = true;
+        this.sucCessMessage = "Thanks for your info!";
+        console.log("submit!");
+        console.log("Name: " + this.myName);
+        console.log("EXP: " + this.exp);
+        return this.sucCess;
+      }
+      
+
+      if (!this.myName) {
+        this.isErr = !this.isErr;
+        this.nameError = "Name REQUIRE!";
+      }
+      if (!this.exp) {
+        this.inputError = "Please input your experiences!";
+      }
+    },
+  },
+};
 </script>
